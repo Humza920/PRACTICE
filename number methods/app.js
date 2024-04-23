afe5fc63a7a87e616c8e2cfd8049d46e855b60d0
Humza920 <humza.rehman.779@gmail.com>
2024-04-23 04:11:54 -0700

    METHODS

---
+++ number methods/app.js
@@ -0,0 +1,16 @@
+// round
+let num = 2.5789
+// console.log(Math.round(num));
+// Math.round mai jis tha math mai round hota hai use trha round hoga lekin agr pooint kai baad 5 hai tou round hokar woh ik bara show hoga jaise 2 .5 hai tou 3 ho jaiga 
+
+// floor
+// console.log(Math.floor(num))
+// iss mai point kai baad wala num chota ho ya bara ho jaise agrr 2.7 hai tou is mai 2 hi rhai ga 3 nhi hoga jabka yr round hokar 3 banta hai 
+
+// ceil
+// console.log(Math.ceil(num))
+// iss mai point kai baad wala num chota ho ya bara ho jaise agrr 2.3 hai tou is mai 3 ho jaiga ga jabka ye round hokar 2 banta hai 
+
+// round
+// console.log(Math.random())
+// iss se koi bhi number randomly generate hoga\
