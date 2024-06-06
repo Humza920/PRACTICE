f1ce7ca599da33ba9718c94b93fe82d50278d6ee
Humza920 <humza.rehman.779@gmail.com>
2024-06-06 07:01:25 -0700

    DONE

--- ASSIGNMENT PRACTICE/main.js
+++ ASSIGNMENT PRACTICE/main.js
@@ -7,9 +7,18 @@ const userName = document.querySelector("#full-name")
 const arr = []
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    
+        event.preventDefault()
+        
+     const data = arr.userName = userName.value
+     const data2 =  arr.email = email.value
+     const data3 =  arr.pass = pass.value
+       
+        userName.value = ""
+        email.value = ""
+        pass.value = ""
 
-})
+        console.log(arr);
 
-console.log(arr);
+    })
+window.localStorage.setItem()
+
