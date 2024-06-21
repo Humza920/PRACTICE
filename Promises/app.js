54d449c8d185c609e34d2fbe4e5e52d51e5eddb4
Humza920 <humza.rehman.779@gmail.com>
2024-06-21 06:28:41 -0700

    CREATED

--- Promises/app.js
+++ Promises/app.js
@@ -5,10 +5,10 @@
 //             console.log("Picnic done");
 //             resolve("DONE")
 //         } else {
-//             console.log("Picnic not done");    
+//             console.log("Picnic not done");
 //             reject("Not done")
 //         }
-//     },1000 
+//     },1000
 // );
 // })
 // picnic.
@@ -23,14 +23,13 @@
 
 // axios('https://fakestoreapi.com/products').
 // then((res)=>{
-//     console.log(res.data); 
+//     console.log(res.data);
 //     res.data.map((item)=>{
 //         div.innerHTML+=`<h1>TITLE: ${item.title}<h1/>
 //         <img width="300" src='${item.image}'/>
 //         <h1>PRICE: ${item.price}<h1/>`
 //     })
 // })
-
 
 // const bankBalance = 200000
 // const shadi = new Promise((resolve, reject) => {
@@ -51,23 +50,36 @@
 //     console.log(err)
 // })
 
+// function shadi (bankBalance) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (bankBalance>200000) {
+//                 console.log("SHADI MUBARAK");
+//             } else {
+//                 console.log("AMA NAI ZEHAR KHA LIA");
+//             }
+//         }, 1000);
+//     })
+// }
 
-function shadi (bankBalance) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (bankBalance>200000) {
-                console.log("SHADI MUBARAK");
-            } else {
-                console.log("AMA NAI ZEHAR KHA LIA");
-            }        
-        }, 1000);
+// shadi(100000)
+// .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+const div = document.querySelector("div");
+
+axios("https://api.escuelajs.co/api/v1/users")
+  .then((res) => {
+    console.log(res.data);
+    res.data.map(item => {
+
     })
-}
+  })
 
-shadi(100000)
-.then((res)=>{
-    console.log(res);
-})
-.catch((err)=>{
+  .catch((err) => {
     console.log(err);
-})
+  });
