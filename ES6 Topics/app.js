c773cea195ec2f22b2027079eb7a29d618e5b9ba
Humza920 <humza.rehman.779@gmail.com>
2024-06-24 05:25:41 -0700

    CREATEED

---
+++ ES6 Topics/app.js
@@ -0,0 +1,47 @@
+// TERNARY OPERATOR
+
+// 1
+// const num1 = 54
+
+// num1>70?
+// console.log("Great"):
+
+// num1>60?
+// console.log("Good"):
+
+// num1>50?
+// console.log("YOU ARE PASSED"):
+
+// num1>40?
+// console.log("YOU ARE FAILED"):
+// console.log("YOU ARE NOT ELIGIBLE FOR EXAM")
+
+// 2
+// const bakery = ["paties","cakes","biscuits","chips","nimco"]
+// const prom = prompt("ENTER YOUR ORDER")
+
+// bakery.includes(prom)?
+// console.log("ORDER RECIEVED"):
+// console.log("SORRY WE DONT HAVE THIS PRODUCT")
+
+// 3
+// let users = [
+//     {
+//         name: "Alice",
+//         age: 30,
+//         city: "New York"
+//     },
+//     {
+//         name: "Bob",
+//         age: 25,
+//         city: "Los Angeles"
+//     }
+// ];
+
+// let user = prompt("ENTER YOUR NAME");
+
+// let foundUser = users.find(u => u.name === user)
+
+// foundUser?
+// console.log("found"):
+// console.log("NOT FOUND")
