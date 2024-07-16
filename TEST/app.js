a14567160fbf7a9c77cc46b7fc30fd9ff490d53f
Humza920 <humza.rehman.779@gmail.com>
2024-07-16 02:58:52 -0700

    DONE

--- TEST/app.js
+++ TEST/app.js
@@ -1,39 +1,80 @@
-const userName = document.querySelector("#names")
-const userAge = document.querySelector("#age")
-const userEmail = document.querySelector("#email")
-const form = document.querySelector("form")
-const input = document.querySelector("input")
-const button = document.querySelector("button")
-const div = document.querySelector("div")
-const Btn = document.querySelector("Btn")
-let arr = []
+const userName = document.querySelector("#names");
+const userAge = document.querySelector("#age");
+const userEmail = document.querySelector("#email");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const div = document.querySelector("div");
+const Btn = document.querySelector("Btn");
+const ol = document.querySelector("ol");
+let arr = [];
 
-form.addEventListener("submit" ,(e)=>{
-    e.preventDefault()
-    let obj = {Name:userName.value,
-        Age:userAge.value,
-        Email:userEmail.value
+// form.addEventListener("submit" ,(e)=>{
+//     e.preventDefault()
+//     let obj = {Name:userName.value,
+//         Age:userAge.value,
+//         Email:userEmail.value
+//     }
+//     userName.value =""
+//     userAge.value =""
+//     userEmail.value =""
+
+//     console.log(obj);
+//     arr.push(obj)
+//     console.log(arr);
+
+//     div.innerHTML += `</br>
+//     <li>${obj.Name}</li>
+//     <li>${obj.Age}</li>
+//     <li>${obj.Email}</li>
+//     <button id ="Btn" onclick="">Clear</button>
+//     `
+
+// // const clear = document.querySelector("#Btn")
+
+// // clear.addEventListener("click",()=>{
+// //     div.innerHTML =""
+// //     });
+// })
+
+// function clear(index) {
+//     arr.splice(index , 1)
+//     console.log(arr);
+
+// }
+
+// console.log(arr);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let obj = {
+    Name: userName.value,
+    Age: userAge.value,
+    Email: userEmail.value,
+  };
+  arr.push(obj);
+
+  userName.value = "";
+  userAge.value = "";
+  userEmail.value = "";
+
+  ol.innerHTML = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    ol.innerHTML += `
+        <li>${arr[i].Name}</li>
+        <li>${arr[i].Age}</li>
+        <li>${arr[i].Email}</li>
+        <button id ="Btn">Clear</button>
+        `;
     }
-    userName.value =""
-    userAge.value =""
-    userEmail.value =""
-
-
-    console.log(obj);
-    arr.push(obj)
     console.log(arr);
-
-    div.innerHTML += `</br>
-    <li>${obj.Name}</li>
-    <li>${obj.Age}</li>
-    <li>${obj.Email}</li>
-    <button id ="Btn" onclick="clear()">Clear</button>
-    `    
-    
-const clear = document.querySelector("#Btn")
-
-clear.addEventListener("click",()=>{
-    div.innerHTML =""
+    const Btn = document.querySelector("#Btn");
+  
+    Btn.addEventListener("click", () => {
+ obj.Name=""
+ obj.Age=""
+ obj.Email=""
     });
-})
 
+});
